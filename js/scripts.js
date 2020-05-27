b4b119387e1cf84a90b609beadac374a72489f04
kelvinmukaria <mukariakelvin17@gmail.com>
2020-05-27 14:38:12 +0300

    Style services section,add linear gradient on background, debug assignining of IDs

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+    $("#oilimg").click(function() {
+        $('#oilimg').slideDown('1500').hide('1000');
+        $("#oil").show('1500');
+    });
+    $("#oil").click(function() {
+        $("#oil").slideUp('1500').hide('1000');
+        $("#oilimg").show('1500');
+    });
+});
+
+$(document).ready(function() {
+    $("#oilimg").click(function() {
+        $('#oilimg').slideDown('1500').hide('1000');
+        $("#oil").show('1500');
+    });
+    $("#oil").click(function() {
+        $("#oil").slideUp('1500').hide('1000');
+        $("#oilimg").show('1500');
+    });
+});
+
+$(document).ready(function() {
+    $("#oilimg").click(function() {
+        $('#oilimg').slideDown('1500').hide('1000');
+        $("#oil").show('1500');
+    });
+    $("#oil").click(function() {
+        $("#oil").slideUp('1500').hide('1000');
+        $("#oilimg").show('1500');
+    });
+});
+
+$(document).ready(function() {
+    $("#oilimg").click(function() {
+        $('#oilimg').slideDown('1500').hide('1000');
+        $("#oil").show('1500');
+    });
+    $("#oil").click(function() {
+        $("#oil").slideUp('1500').hide('1000');
+        $("#oilimg").show('1500');
+    });
+});
+
+$(document).ready(function() {
+    $("#oilimg").click(function() {
+        $('#oilimg').slideDown('1500').hide('1000');
+        $("#oil").show('1500');
+    });
+    $("#oil").click(function() {
+        $("#oil").slideUp('1500').hide('1000');
+        $("#oilimg").show('1500');
+    });
+});
+
+$(document).ready(function() {
+    $("#oilimg").click(function() {
+        $('#oilimg').slideDown('1500').hide('1000');
+        $("#oil").show('1500');
+    });
+    $("#oil").click(function() {
+        $("#oil").slideUp('1500').hide('1000');
+        $("#oilimg").show('1500');
+    });
+});
