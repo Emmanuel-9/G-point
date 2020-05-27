b403a6d4d913f21809ee1aac61d3e429bcc87d87
kelvinmukaria <mukariakelvin17@gmail.com>
2020-05-27 16:37:42 +0300

    Add javascript functions to enable toggle between images and icons in services section

--- js/scripts.js
+++ js/scripts.js
@@ -10,56 +10,56 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-    $("#oilimg").click(function() {
-        $('#oilimg').slideDown('1500').hide('1000');
-        $("#oil").show('1500');
+    $("#airimg").click(function() {
+        $('#airimg').slideDown('1500').hide('1000');
+        $("#air").show('1500');
     });
-    $("#oil").click(function() {
-        $("#oil").slideUp('1500').hide('1000');
-        $("#oilimg").show('1500');
-    });
-});
-
-$(document).ready(function() {
-    $("#oilimg").click(function() {
-        $('#oilimg').slideDown('1500').hide('1000');
-        $("#oil").show('1500');
-    });
-    $("#oil").click(function() {
-        $("#oil").slideUp('1500').hide('1000');
-        $("#oilimg").show('1500');
+    $("#air").click(function() {
+        $("#air").slideUp('1500').hide('1000');
+        $("#airimg").show('1500');
     });
 });
 
 $(document).ready(function() {
-    $("#oilimg").click(function() {
-        $('#oilimg').slideDown('1500').hide('1000');
-        $("#oil").show('1500');
+    $("#coolimg").click(function() {
+        $('#coolimg').slideDown('1500').hide('1000');
+        $("#cool").show('1500');
     });
-    $("#oil").click(function() {
-        $("#oil").slideUp('1500').hide('1000');
-        $("#oilimg").show('1500');
-    });
-});
-
-$(document).ready(function() {
-    $("#oilimg").click(function() {
-        $('#oilimg').slideDown('1500').hide('1000');
-        $("#oil").show('1500');
-    });
-    $("#oil").click(function() {
-        $("#oil").slideUp('1500').hide('1000');
-        $("#oilimg").show('1500');
+    $("#cool").click(function() {
+        $("#cool").slideUp('1500').hide('1000');
+        $("#coolimg").show('1500');
     });
 });
 
 $(document).ready(function() {
-    $("#oilimg").click(function() {
-        $('#oilimg').slideDown('1500').hide('1000');
-        $("#oil").show('1500');
+    $("#brakeimg").click(function() {
+        $('#brakeimg').slideDown('1500').hide('1000');
+        $("#brakes").show('1500');
     });
-    $("#oil").click(function() {
-        $("#oil").slideUp('1500').hide('1000');
-        $("#oilimg").show('1500');
+    $("#brakes").click(function() {
+        $("#brakes").slideUp('1500').hide('1000');
+        $("#brakeimg").show('1500');
+    });
+});
+
+$(document).ready(function() {
+    $("#serviceimg").click(function() {
+        $('#serviceimg').slideDown('1500').hide('1000');
+        $("#service").show('1500');
+    });
+    $("#service").click(function() {
+        $("#service").slideUp('1500').hide('1000');
+        $("#serviceimg").show('1500');
+    });
+});
+
+$(document).ready(function() {
+    $("#sevenimg").click(function() {
+        $('#sevenimg').slideDown('1500').hide('1000');
+        $("#seven").show('1500');
+    });
+    $("#seven").click(function() {
+        $("#seven").slideUp('1500').hide('1000');
+        $("#sevenimg").show('1500');
     });
 });
